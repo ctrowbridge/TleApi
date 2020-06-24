1e7a74d7ea076b4d8fb3f3f69db0ea4d54b775af
Cindy Trowbridge <clynndyt@gmail.com>
2020-06-24 14:56:56 -0600

    Initial commit

---
+++ src/main/java/com/cindy/tleapi/astro/Angle.java
@@ -0,0 +1,34 @@
+package com.cindy.tleapi.astro;
+
+/**
+ * Represents one Angle, with units specified
+ */
+public class Angle {
+
+    public enum AngleUnits {DEGREES, RADIANS}
+
+    private double valueRad;
+
+    public Angle(double value, AngleUnits units) {
+        if (units == AngleUnits.RADIANS) {
+            this.valueRad = value;
+        } else {
+            this.valueRad = Math.toRadians(value);
+        }
+    }
+
+    public double getDegrees() {
+        return Math.toDegrees(valueRad);
+    }
+
+    public double getRadians() {
+        return valueRad;
+    }
+
+    @Override
+    public String toString() {
+        return "Angle{" +
+                "valueRad=" + valueRad +
+                '}';
+    }
+}
